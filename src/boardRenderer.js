508aa57e3d484835e50f5a98eaff2ea3735fce41
AgnieszkaKurek <agniechakurek@gmail.com>
2018-09-10 16:04:08 +0200

    correct table

--- src/boardRenderer.js
+++ src/boardRenderer.js
@@ -1,22 +1,18 @@
 import $ from 'jquery';
 export class BoardRenderer {
     constructor() {
-
+        this.numberOfElements = 11;
     }
 
     initBoard() {
-        let table = '';
-        table += '<table ></table>';
-        $('#board').html(table);
-        let tr = '';
-        for (let i = 0; i < 10; i++) {
-            tr += '<tr></tr>';
-            $('table').html(tr);
-            let td = '';
-            for (let j = 0; j < 10; j++) {
-                td += '<td></td>';
-                $('tr').html(td);
-            }
+        let tableConstructor = '';
+        let columns = '';
+        for (let i = 0; i < this.numberOfElements; i++) {
+            tableConstructor += `<tr>${columns}</tr>`;
+            columns = '<td></td>';
+            columns = columns.repeat(this.numberOfElements);
         }
+        let table = `<table>${tableConstructor}</table>`;
+        $('#board').html(table);
     }
-}
+} 
