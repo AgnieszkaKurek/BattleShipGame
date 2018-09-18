63783ea57626b072ad03dd026e98999301f4087d
AgnieszkaKurek <agniechakurek@gmail.com>
2018-09-18 11:12:56 +0200

    adding attributes to table

--- src/boardRenderer.js
+++ src/boardRenderer.js
@@ -7,10 +7,20 @@ export class BoardRenderer {
 
     initBoard() {
         let rows = '';
-        for (let rowIndex = 0; rowIndex < this.numberOfRowsAndColumns; rowIndex++) {
+        for (let rowIndex = 0; rowIndex < this.numberOfRowsAndColumns + 1; rowIndex++) {
             let columns = '';
-            for (let columnIndex = 0; columnIndex < this.numberOfRowsAndColumns; columnIndex++) {
-                columns += `<td data-position-row="${rowIndex}" data-position-column="${columnIndex}"></td>`;
+            let state;
+            for (let columnIndex = 0; columnIndex < this.numberOfRowsAndColumns + 1; columnIndex++) {
+                if (columnIndex === 0 && rowIndex === 0) {
+                    state = 'invisibleBox';
+                }
+                else if (columnIndex === 0 || rowIndex === 0) {
+                    state = 'boxHeader';
+                }
+                else {
+                    state = 'basicBox';
+                }
+                columns += `<td data-position-row="${rowIndex}" data-position-column="${columnIndex}" data-state= "${state}"></td>`;
             }
             rows += `<tr>${columns}</tr>`;
         }
