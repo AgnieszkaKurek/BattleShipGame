26d82ce5ac9fa7386a96b49d08d117e5ca2d3d6a
AgnieszkaKurek <agniechakurek@gmail.com>
2019-02-21 16:11:19 +0100

    creating  new  array

--- src/battleShipGame.js
+++ src/battleShipGame.js
@@ -1,3 +1,10 @@
 export class BattleShipGame {
 
-}
+ constructor(){
+        this.creatBoard();
+    }
+ creatBoard(){
+    this.board = [...Array(10)].map(x=>Array(10).fill(0));
+     
+    }
+} 
--- tests/battleShipGameSpec.js
+++ tests/battleShipGameSpec.js
@@ -9,5 +9,22 @@ describe('BattleShipGame', () => {
     it('Given created game, when getting its instance, then it is defined', () => {
         expect(game).toBeDefined();
     });
+    it('Given game, when game begins, then contains a board', () => {
+        expect(game.board).toBeDefined();
+    });
+    it('Given game, when game begins, then board is of 9 empty elements', () => {
+        expect(game.board).toEqual([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]);
 
+    });
 });
